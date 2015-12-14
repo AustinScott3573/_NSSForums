a1902f899fd77c5ed252782b220c211114ccdab2
AustinScott3573 <Ascott3573@gmail.com>
2015-12-14 10:02:08 -0600

    Added board.js with the comment function. knockoutjs is pretty darn cool

---
+++ _NssForums/Scripts/board.js
@@ -0,0 +1,42 @@
+﻿var post = function (id, message, username, date) {
+    this.id = id;
+    this.message = message;
+    this.username = username;
+    this.date = date;
+    this.comments = ko.observableArray([]);
+
+    this.addComment = function (context) {
+        var comment = $('input[name="comment"]', context).val();
+        if (comment.lengh > 0) {
+            $.connection.boardHub.server.addComment(this.id, comment, vm.username())
+            .done(function () {
+                $('input[name="comment"]', conext).val('');
+            });
+        }
+    };
+}
+
+var comment = function (id, message, unsername, date) {
+    this.id = id;
+    this.message = message;
+    this.username = username;
+    this.date = date;
+}
+
+var vm = {
+    posts: ko.observableArray([]),
+    notifications: ko.observableArray([]),
+    username: ko.observableArray(),
+    signedIn:  ko.observable(false),
+    signIn: function () {
+        vm.username($('#username').val());
+        vm.signedIn(true);
+    },
+    writePost: function () {
+        $.connection.boardHub.server.writePost(vm.username(), $('#message'). val()).done(function () {
+            $('#message').val('');
+        });
+    },
+  }
+
+ko.applyBindings(vm);
