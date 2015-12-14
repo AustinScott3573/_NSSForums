53636befcf40200acbd06c83ae213ab478b5b59e
AustinScott3573 <Ascott3573@gmail.com>
2015-12-14 13:16:27 -0600

    Added more dependencies because the author didn't, wierd. Also, board.js is done and you can now sign in as a mock user.

--- _NssForums/Scripts/board.js
+++ _NssForums/Scripts/board.js
@@ -7,16 +7,16 @@
 
     this.addComment = function (context) {
         var comment = $('input[name="comment"]', context).val();
-        if (comment.lengh > 0) {
+        if (comment.length > 0) {
             $.connection.boardHub.server.addComment(this.id, comment, vm.username())
             .done(function () {
-                $('input[name="comment"]', conext).val('');
+                $('input[name="comment"]', context).val('');
             });
         }
     };
 }
 
-var comment = function (id, message, unsername, date) {
+var comment = function (id, message, username, date) {
     this.id = id;
     this.message = message;
     this.username = username;
@@ -26,20 +26,18 @@ var comment = function (id, message, unsername, date) {
 var vm = {
     posts: ko.observableArray([]),
     notifications: ko.observableArray([]),
-    username: ko.observableArray(),
-    signedIn:  ko.observable(false),
+    username: ko.observable(),
+    signedIn: ko.observable(false),
     signIn: function () {
         vm.username($('#username').val());
         vm.signedIn(true);
     },
     writePost: function () {
-        $.connection.boardHub.server.writePost(vm.username(), $('#message'). val()).done(function () {
+        $.connection.boardHub.server.writePost(vm.username(), $('#message').val()).done(function () {
             $('#message').val('');
         });
     },
-  }
-
-ko.applyBindings(vm);
+}
 
 ko.applyBindings(vm);
 
@@ -57,3 +55,37 @@ function loadPosts() {
         });
     });
 }
+
+$(function () {
+    var hub = $.connection.boardHub;
+    $.connection.hub.start().done(function () {
+        loadPosts(); // Load posts when connected to hub
+    });
+
+    // Hub calls this after a new post has been added
+    hub.client.receivedNewPost = function (id, username, message, date) {
+        var newPost = new post(id, message, username, date);
+        vm.posts.unshift(newPost);
+
+        // If another user added a new post, add it to the activity summary
+        if (username !== vm.username()) {
+            vm.notifications.unshift(username + ' has added a new post.');
+        }
+    };
+
+    // Hub calls this after a new comment has been added
+    hub.client.receivedNewComment = function (parentPostId, commentId, message, username, date) {
+        // Find the post object in the observable array of posts
+        var postFilter = $.grep(vm.posts(), function (p) {
+            return p.id === parentPostId;
+        });
+        var thisPost = postFilter[0]; //$.grep returns an array, we just want the first object
+
+        var thisComment = new comment(commentId, message, username, date);
+        thisPost.comments.push(thisComment);
+
+        if (thisPost.username === vm.username() && thisComment.username !== vm.username()) {
+            vm.notifications.unshift(username + ' has commented on your post.');
+        }
+    };
+});
