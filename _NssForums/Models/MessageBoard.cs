2893a3357687f09df02c6f5a008776149ce42ac5
AustinScott3573 <Ascott3573@gmail.com>
2015-12-17 18:04:11 -0600

    Revert "fixed commited files"
    This reverts commit 42971a9d9662d2e82a05ed0f3bfbd35018c0059a.

--- _NssForums/Models/MessageBoard.cs
+++ _NssForums/Models/MessageBoard.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Web;
 namespace _NssForums.Models
 {
-    public class MessageBoardContext : ApplicationDbContext
+    public class MessageBoardContext : DbContext
     {
         public DbSet<Post> Posts { get; set; }
         public DbSet<Comment> Comments { get; set; }
